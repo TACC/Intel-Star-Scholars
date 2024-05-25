50147a8ee7f1f2b3f8cccd4f8c29154f953890fa
Yojan Chitkara <yojanchitkara04@gmail.com>
2024-05-24 22:14:47 -0500

    Run script to enable SYCL stencil scaling study

---
+++ SYCL/STENCIL/StencilScalingStudySPR.sh
@@ -0,0 +1,42 @@
+export DPCPP_CPU_NUM_CUS=1
+export DPCPP_CPU_CU_AFFINITY=close
+export DPCPP_CPU_PLACES=cores
+./simulate13 10 cpu 40000 40000
+export DPCPP_CPU_NUM_CUS=8
+./simulate13 10 cpu 40000 40000
+export DPCPP_CPU_NUM_CUS=16
+./simulate13 10 cpu 40000 40000
+export DPCPP_CPU_NUM_CUS=24
+./simulate13 10 cpu 40000 40000
+export DPCPP_CPU_NUM_CUS=32
+./simulate13 10 cpu 40000 40000
+export DPCPP_CPU_NUM_CUS=40
+./simulate13 10 cpu 40000 40000
+export DPCPP_CPU_NUM_CUS=48
+./simulate13 10 cpu 40000 40000
+export DPCPP_CPU_NUM_CUS=56
+./simulate13 10 cpu 40000 40000
+export DPCPP_CPU_NUM_CUS=64
+./simulate13 10 cpu 40000 40000
+export DPCPP_CPU_NUM_CUS=72
+./simulate13 10 cpu 40000 40000
+export DPCPP_CPU_NUM_CUS=80
+./simulate13 10 cpu 40000 40000
+#export DPCPP_CPU_NUM_CUS=24
+#./simulate13 10 cpu 40000 40000
+#export DPCPP_CPU_NUM_CUS=26
+#./simulate13 10 cpu 40000 40000
+#export DPCPP_CPU_NUM_CUS=28
+#./simulate13 10 cpu 40000 40000
+#export DPCPP_CPU_NUM_CUS=30
+#./simulate13 10 cpu 40000 40000
+#export DPCPP_CPU_NUM_CUS=32
+#./simulate13 10 cpu 40000 40000
+#export DPCPP_CPU_NUM_CUS=34
+#./simulate13 10 cpu 40000 40000
+#export DPCPP_CPU_NUM_CUS=36
+#./simulate13 10 cpu 40000 40000
+#export DPCPP_CPU_NUM_CUS=38
+#./simulate13 10 cpu 40000 40000
+#export DPCPP_CPU_NUM_CUS=40
+#./simulate13 10 cpu 40000 40000
