1939b5bcd339f64c742540016171f663cacf9878
Yojan Chitkara <yojanchitkara04@gmail.com>
2024-12-08 22:05:37 -0600

    Updated Working CUDA Code - Explicit copy

--- CUDA/Stencil.cu
+++ CUDA/Stencil.cu
@@ -61,5 +61,104 @@ __global__ void stencil_kernel(const float *Mat_A, float *Mat_Stencil, int N, in
     }
 }
 
+// Kernel to compute sqrt of FNorm **once**
+__global__ void compute_sqrt(float *FNorm) {
+    *FNorm = sqrtf(*FNorm);  // Perform single square root
+}
+
+// Kernel to normalize the matrix
+__global__ void normalize_kernel(float *Mat_A, const float *Mat_Stencil, float *FNorm, int N, int M) {
+    int row = blockIdx.y * blockDim.y + threadIdx.y + 1;
+    int col = blockIdx.x * blockDim.x + threadIdx.x + 1;
+
+    if (row < N - 1 && col < M - 1) {
+        Mat_A[row * M + col] = Mat_Stencil[(row - 1) * (M - 2) + (col - 1)] / *FNorm;
+    }
+}
+
+int main(int argc, char *argv[]) {
+
+    int loops = atoi(argv[1]);
+    int N = atoi(argv[2]);
+    int M = atoi(argv[3]);
+
+    unsigned long long int ClkPerSec;
+    double NSecClk;
+
+    std::vector<float> Mat_A(N * M, 1.0f);
+    std::vector<float> Mat_Stencil((N - 2) * (M - 2), 0.0f);
+    float FNorm = 0.0f;
+    double elapsed_time[10], Average = 0.0;
+
+    Calibrate(&ClkPerSec, NSecClk);
+
+    // Allocate device memory
+    float *d_Mat_A, *d_Mat_Stencil, *d_FNorm;
+    cudaMalloc(&d_Mat_A, N * M * sizeof(float));
+    cudaMalloc(&d_Mat_Stencil, (N - 2) * (M - 2) * sizeof(float));
+    cudaMalloc(&d_FNorm, sizeof(float));
 
 
+    dim3 threadsPerBlock(16, 16);
+    dim3 numBlocks((M + threadsPerBlock.x - 1) / threadsPerBlock.x,
+                   (N + threadsPerBlock.y - 1) / threadsPerBlock.y);
+
+    for (int count = 0; count < loops; count++) {
+
+        if(FNorm == 0.0f)
+          FNorm = 1.0f;
+        else
+          FNorm = 1.0f;
+        unsigned long long start = rdtsc();
+        cudaMemcpy(d_FNorm, &FNorm, sizeof(float), cudaMemcpyHostToDevice);
+        cudaMemcpy(d_Mat_A, Mat_A.data(), N * M * sizeof(float), cudaMemcpyHostToDevice);
+
+
+        // Launch the stencil kernel
+        stencil_kernel<<<numBlocks, threadsPerBlock>>>(d_Mat_A, d_Mat_Stencil, N, M, d_FNorm);
+        cudaDeviceSynchronize();
+
+        // Perform single sqrt calculation
+        compute_sqrt<<<1, 1>>>(d_FNorm);  // Single thread kernel
+        cudaDeviceSynchronize();
+
+        // Normalize the matrix
+        normalize_kernel<<<numBlocks, threadsPerBlock>>>(d_Mat_A, d_Mat_Stencil, d_FNorm, N, M);
+        cudaDeviceSynchronize();
+
+        cudaMemcpy(Mat_A.data(), d_Mat_A, N * M * sizeof(float), cudaMemcpyDeviceToHost);
+        cudaMemcpy(&FNorm, d_FNorm, sizeof(float), cudaMemcpyDeviceToHost);
+        unsigned long long end = rdtsc();
+
+        /*for(int i=0;i<N*M;i+=M)
+        {
+            for(int j=i;j<(i+M);j++)
+                printf("%f ",Mat_A[j]);
+            printf("\n");
+        }*/
+
+        elapsed_time[count] = (double)(end - start) / ClkPerSec;
+        printf("TTC : %.12f\n", elapsed_time[count]);
+    }
+
+
+    /*for(int i=0;i<N*M;i+=M)
+    {
+        for(int j=i;j<(i+M);j++)
+            printf("%f ",Mat_A[j]);
+        printf("\n");
+    }*/ 
+
+    // Print average time
+    for (int count = 1; count < loops; count++) {
+        Average += elapsed_time[count];
+    }
+    Average /= (loops - 1);
+    printf("Average Computation Time: %.12f\n", Average);
+
+    cudaFree(d_Mat_A);
+    cudaFree(d_Mat_Stencil);
+    cudaFree(d_FNorm);
+
+    return 0;
+}
